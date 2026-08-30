3a78e4da5e578f797f4b935370c4a850ee3f3f3a
Felix Yeung <19355619+imfelixyeung@users.noreply.github.com>
2026-08-30 23:53:13 +0100

    feat: redirect root

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,16 @@
 import type { NextConfig } from "next";
+import { defaultLocale } from "./src/i18n";
 
-const nextConfig: NextConfig = {/* config options here */};
+const nextConfig: NextConfig = {
+    redirects() {
+        return [
+            {
+                source: "/",
+                destination: `/${defaultLocale}`,
+                permanent: true,
+            },
+        ];
+    },
+};
 
 export default nextConfig;
